10491b3470e78eca62b59fb6f5af10f602b3e122
Davide Madrisan <davide.madrisan@gmail.com>
2022-05-09 16:22:31 +0200

    Update the Copyleft notice
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- cmd/get.go
+++ cmd/get.go
@@ -1,5 +1,5 @@
 /*
-  Copyright 2021 Davide Madrisan <davide.madrisan@gmail.com>
+  Copyright 2021-2022 Davide Madrisan <davide.madrisan@gmail.com>
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
